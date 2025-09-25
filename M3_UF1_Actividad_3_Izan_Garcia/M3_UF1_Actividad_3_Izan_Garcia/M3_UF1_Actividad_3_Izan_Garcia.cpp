82b9c63a6ced0c3b20d3ab8e47ba969e71882a23
0xIGL <izan.garcia2007@gmail.com>
2025-09-25 18:46:23 +0200

    actividad 3 hasta el ex6

--- M3_UF1_Actividad_3_Izan_Garcia/M3_UF1_Actividad_3_Izan_Garcia/M3_UF1_Actividad_3_Izan_Garcia.cpp
+++ M3_UF1_Actividad_3_Izan_Garcia/M3_UF1_Actividad_3_Izan_Garcia/M3_UF1_Actividad_3_Izan_Garcia.cpp
@@ -45,6 +45,7 @@ int main()
 
 /*Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.*/
 
+/*
 int main()
 {
 	int num = 5;
@@ -60,5 +61,181 @@ int main()
 
 	return 0;
 }
+*/
 
+// Ejercicio 4
 
+/*Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.*/
+
+/*
+int main()
+{
+	int num = 320;
+	int resultado;
+
+	for (int i = 0; i <= 8; i++)
+	{
+		resultado = num - 20 * i;
+		printf("Resultado: %d\n", resultado);
+	}
+
+	return 0;
+}
+*/
+
+// Ejercicio 4 Version escalable (He pensado otra version del codigo pero para que sea mas escalable y poder modificar los valores)
+
+/*Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.*/
+
+/*
+int main()
+{
+	int num_inicio = 320;
+	int num_final = 160;   // añadimos num final como variable para poder modificarlo
+	int contador = 20;	   // añadimos la variable contador para poder modificar la cantidad de los saltos.
+	
+	printf("Introduce el numero de inicio: ");
+	scanf_s("%d", &num_inicio);
+
+	printf("Introduce el numero final: ");
+	scanf_s("%d", &num_final);
+
+	printf("Introduce de cuanto en cuanto quieres contar: ");
+	scanf_s("%d", &contador);
+
+	if (contador <= 0)          // Pongo que contador no puede ser 0 o menos porque si no nunca acabaria el bucle y mostraria todo el rato el numero de inicio, ademas por el orden de prioridad que vimos en clase tiene que estar la primera, si no entraria a los otros casos     
+	{
+		printf("Contador no puede ser igual o mas pequenyo que 0\n");
+	}
+	else if (num_inicio > num_final)
+	{
+		for (int i = num_inicio; i >= num_final; i -= contador)
+		{
+			printf("Resultado: %d\n", i);
+		}                                                                           // hacemos un if para saber que numero es mas grande y poder contar de arriba a abajo o viceversa)
+	}
+	else if (num_inicio < num_final)
+	{
+		for (int i = num_inicio; i <= num_final; i += contador)       
+		{
+			printf("Resultado: %d\n", i);
+		}
+	}                                                                     
+	else
+	{
+		printf("Los numeros son iguales\n");
+	}
+
+	return 0;
+}
+*/
+
+// ejercicio 5   (lo voy a hacer escalable como el otro)
+
+/*Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle while.*/
+
+/*
+int main()
+{
+	int num_inicio;
+	int num_final;
+	int contador;
+
+	printf("Introduce el numero de inicio: ");
+	scanf_s("%d", &num_inicio);
+
+	printf("Introduce el numero final: ");
+	scanf_s("%d", &num_final);
+
+	printf("Introduce de cuanto en cuanto quieres contar: ");
+	scanf_s("%d", &contador);
+
+	if (contador <= 0)
+	{
+		printf("Contador no puede ser igual o mas pequenyo que 0\n");
+	}
+	else if (num_inicio > num_final)
+	{
+		while (num_inicio >= num_final)
+		{
+			printf("Resultado: %d\n", num_inicio);
+			num_inicio -= contador;
+		}
+	}
+	else if (num_inicio < num_final)
+	{
+		while (num_inicio <= num_final)
+		{
+			printf("Resultado: %d\n", num_inicio);
+			num_inicio += contador;
+		}
+	}
+	else
+	{
+		printf("Los numeros son iguales\n");
+	}
+
+	return 0;
+}
+*/
+
+// Ejercicio 6
+
+/*Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while.*/
+
+/*
+int main()
+{
+	int num_inicio;
+	int num_final;
+	int contador;
+
+	printf("Introduce el numero de inicio: ");
+	scanf_s("%d", &num_inicio);
+
+	printf("Introduce el numero final: ");
+	scanf_s("%d", &num_final);
+
+	printf("Introduce de cuanto en cuanto quieres contar: ");
+	scanf_s("%d", &contador);
+
+	if (contador <= 0)
+	{
+		printf("Contador no puede ser igual o mas pequenyo que 0\n");
+	}
+	else if (num_inicio > num_final)
+	{
+		do
+		{
+			printf("Resultado: %d\n", num_inicio);
+			num_inicio -= contador;
+		} while (num_inicio >= num_final);
+	}
+	else if (num_inicio < num_final)
+	{
+		do
+		{
+			printf("Resultado: %d\n", num_inicio);
+			num_inicio += contador;
+		} while (num_inicio <= num_final);
+	}
+	else
+	{
+		printf("Los numeros son iguales\n");
+	}
+
+	return 0;
+}
+*/
+
+// Ejercicio 7
+
+/*Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El
+programa nos pedirá la combinación para abrirla. Si no acertamos, se nos mostrará el
+mensaje “Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha        
+abierto satisfactoriamente”.*/
+
+int main()
+{
+	
+}
