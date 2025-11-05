0a70d7d7e2b5d44cf01ba5c8bfe5aa63ea18e0bb
0xIGL <izan.garcia2007@gmail.com>
2025-11-05 23:09:00 +0100

    ACT1UF2 Create

---
+++ Izan_Garcia_ACT1UF2/Izan_Garcia_ACT1UF2/Izan_Garcia_ACT1UF2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void Ejercicio1();
+void Par();
+
+int main()
+{
+	//Ejercicio1();
+	//Ejercicio2();
+	//Ejercicio3();
+}
+
+void Ejercicio1()                // Por parametro es pedir numero y que la funcion lo reciba
+{
+	Par();
+}
+
+void Par()
+{
+	//Logica
+}
