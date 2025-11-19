a9342027ff384eaa4aa37d001611347d0f68eda6
0xIGL <izan.garcia2007@gmail.com>
2025-11-19 16:45:50 +0100

    funciones por valor y referencia terminado

--- Garcia_Izan_Funciones_por_valor_y_referencia/Garcia_Izan_Funciones_por_valor_y_referencia/Garcia_Izan_Funciones_por_valor_y_referencia.cpp
+++ Garcia_Izan_Funciones_por_valor_y_referencia/Garcia_Izan_Funciones_por_valor_y_referencia/Garcia_Izan_Funciones_por_valor_y_referencia.cpp
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Ejercicio 1 
-/*
+
+void Ejercicio1();
+void Ejercicio2();
+void Ejercicio3();
 void Operacion(int n1, int n2, char oper, float *resul, int *error);
+void Operacion2(int n1, int n2, char oper, float* resul, int* error, int* fact1, int* fact2);
+int Menu();
+int Menu2();
+int Factorial(int num);
 
 int main()
 {
+	//Ejercicio1();
+	//Ejercicio2();
+	//Ejercicio3();
+	
+}
 
+void Ejercicio1()
+{
 	int n1, n2;
 	char oper;
 	float resultado;
@@ -27,11 +40,114 @@ int main()
 	printf("s --> Salir\n");
 	printf("Elija opcion: ");
 	scanf_s(" %c", &oper, 1);
-	if (oper == 's') return 0;
+	if (oper == 's') return;
 
 	Operacion(n1, n2, oper, &resultado, &error);
-	
+
 	if (error != -1)
+	{
+		printf("\nEl resultado es: %.2f\n", resultado);
+	}
+	else
+	{
+		printf("\nOperacion no valida!\n");
+	}
+}
+
+void Ejercicio2()
+{
+	int n1, n2;
+	char oper =' ';
+	float resultado;
+	int error;
+
+	printf("Dime 2 numeros enteros: ");        
+	scanf_s("%d %d", &n1, &n2);
+	system("cls");
+
+	switch (Menu())
+	{
+	case 1:
+		oper = '+';
+		break;
+	case 2:
+		oper = '-';
+		break;
+	case 3:
+		oper = '*';
+		break;
+	case 4:
+		oper = '/';
+		break;
+	case 5:
+		oper = '^';
+		break;
+	case 0:
+		return;
+	case -1:
+		printf("\nOperacion no valida!\n");
+		return;
+	}
+
+	Operacion(n1, n2, oper, &resultado, &error);
+
+	if (error != -1)
+	{
+		printf("\nEl resultado es: %.2f\n", resultado);
+	}
+	else
+	{
+		printf("\nOperacion no valida!\n");
+	}
+}
+
+void Ejercicio3()
+{
+	int n1, n2;
+	char oper = ' ';
+	float resultado;
+	int error;
+	int fact1, fact2;
+
+	printf("Dime 2 numeros enteros: ");
+	scanf_s("%d %d", &n1, &n2);
+	system("cls");
+
+	switch (Menu2())
+	{
+	case 1:
+		oper = '+';
+		break;
+	case 2:
+		oper = '-';
+		break;
+	case 3:
+		oper = '*';
+		break;
+	case 4:
+		oper = '/';
+		break;
+	case 5:
+		oper = '^';
+		break;
+	case 6:
+		oper = '!';
+		break;
+	case 0:
+		return;
+	case -1:
+		printf("\nOperacion no valida!\n");
+		return;
+	}
+
+	Operacion2(n1, n2, oper, &resultado, &error, &fact1, &fact2);
+
+    if (oper == '!')
+	{
+		printf("\nEl factorial de %d es: %d\n", n1, fact1);
+		printf("\nEl factorial de %d es: %d\n", n2, fact2);
+	}
+	else if (error != -1)
 	{
 		printf("\nEl resultado es: %.2f\n", resultado);
 	}
@@ -66,60 +182,6 @@ void Operacion(int n1, int n2, char oper, float *resul, int *error)
 		default:
 			*error = -1;
 			break;
-	}
-}
-*/
-
-/*
-// Ejercicio 2
-int Menu();
-void Operacion(int n1, int n2, char oper, float *resul, int *error);
-
-int main()
-{
-
-	int n1, n2;
-	char oper;
-	float resultado;
-	int error;
-
-	printf("Dime 2 numeros enteros: ");        //validacion dentro de scanf == 0?
-	scanf_s("%d %d", &n1, &n2);
-	system("cls");
-
-	switch (Menu())
-	{
-		case 1:
-			oper = '+';
-			break;
-		case 2:
-			oper = '-';
-			break;
-		case 3:
-			oper = '*';
-			break;
-		case 4:
-			oper = '/';
-			break;
-		case 5:
-			oper = '^';
-			break;
-		case 0:
-			return 0;
-		case -1:
-			printf("\nOperacion no valida!\n");
-			return 1;
-	} 
-
-	Operacion(n1, n2, oper, &resultado, &error);
-
-	if (error != -1)
-	{
-		printf("\nEl resultado es: %.2f\n", resultado);
-	}
-	else
-	{
-		printf("\nOperacion no valida!\n");
 	}
 }
 
@@ -156,31 +218,84 @@ int Menu()
 	}
 }
 
-void Operacion(int n1, int n2, char oper, float *resul, int *error)
+void Operacion2(int n1, int n2, char oper, float* resul, int* error, int* fact1, int* fact2)
 {
 	*error = 1;
 
 	switch (oper)
 	{
-		case '+':
-			*resul = n1 + n2;
-			break;
-		case '-':
-			*resul = n1 - n2;
-			break;
-		case '*':
-			*resul = n1 * n2;
-			break;
-		case '/':
-			if (n2 == 0) *error = -1;
-			else *resul = (float)n1 / n2;
-			break;
-		case '^':
-			*resul = pow(n1, n2);
-			break;
-		default:
-			*error = -1;
-			break;
+	case '+':
+		*resul = n1 + n2;
+		break;
+	case '-':
+		*resul = n1 - n2;
+		break;
+	case '*':
+		*resul = n1 * n2;
+		break;
+	case '/':
+		if (n2 == 0) *error = -1;
+		else *resul = (float)n1 / n2;
+		break;
+	case '^':
+		*resul = pow(n1, n2);
+		break;
+	case '!':
+		*fact1 = Factorial(n1);
+		*fact2 = Factorial(n2);
+		break;
+	default:
+		*error = -1;
+		break;
 	}
 }
-*/
+
+int Menu2()
+{
+	char oper;
+
+	printf("MENU\n");
+	printf("+ --> Sumar\n");
+	printf("- --> Restar\n");
+	printf("* --> Multiplicar\n");
+	printf("/ --> Dividir\n");
+	printf("^ --> Potencia\n");
+	printf("! --> Factorial\n");
+	printf("s --> Salir\n");
+	printf("Elija opcion: ");
+	scanf_s(" %c", &oper, 1);
+
+	switch (oper)
+	{
+	case '+':
+		return 1;
+	case '-':
+		return 2;
+	case '*':
+		return 3;
+	case '/':
+		return 4;
+	case '^':
+		return 5;
+	case '!':
+		return 6;
+	case 's':
+		return 0;
+	default:
+		return -1;
+	}
+}
+
+int Factorial(int num)
+{
+	int fact = 1;
+
+	for (int i = 1; i <= num; i++)
+	{	
+		fact *= i;
+	}
+
+	return fact;
+}
+
+
